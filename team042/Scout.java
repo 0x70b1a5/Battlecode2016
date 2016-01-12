497017bfe97a4db6f641b7816228202b54e743e9
Brant <bpbiskup@hotmail.com>
2016-01-11 22:29:12 -0500

    Scout code added. Untested, probably doesn't work well

--- team042/Scout.java
+++ team042/Scout.java
@@ -1,21 +1,74 @@
 package team042;
 
-import battlecode.common.*;
+import java.util.Random;
+
+import battlecode.common.Clock;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
+import team042.Utilities;
 
 public class Scout {
 
-	public Scout(RobotController rc) {
+	public static RobotController rc;
+	public static Utilities utils;
+	public static RobotType rt;
+	
+	public Scout(RobotController robotController) {
 		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public static RobotType type() {
-		// TODO Auto-generated method stub
-		return null;
+		rc = robotController;
+		utils = new Utilities(rc);
+		rt = rc.getType();
 	}
 
 	public void run() {
-		// TODO Auto-generated method stub
+		try{
+			Random rand = new Random(rc.getID());
+			int senseRange = rt.sensorRadiusSquared;
+			while(true){
+				RobotInfo enemyArchon = null;
+				MapLocation myLoc = rc.getLocation();
+				RobotInfo[] enemies= rc.senseHostileRobots(myLoc, senseRange);
+				RobotInfo[] friends= rc.senseNearbyRobots(senseRange);
+				int friendCount = friends.length;
+				int enemyCount = enemies.length;
+				
+				//Search for enemy archons. Broadcast the x,y location if found.
+				//TODO: Adjust broadcast range.
+				for(RobotInfo bot : enemies){
+					if(bot.type == RobotType.ARCHON){
+						MapLocation archLoc = bot.location;
+						rc.broadcastMessageSignal(archLoc.x, archLoc.y, 500);
+						enemyArchon = bot;
+						break;
+					}
+					else if(myLoc.distanceSquaredTo(bot.location) < 25){
+						utils.tryMove(myLoc.directionTo(bot.location).opposite());
+					}
+				}
+				
+				//IF core is ready and an enemy Archon was detected:
+				//Initiate stake-out protocols
+				if(rc.isCoreReady() && enemyArchon != null){
+					if(myLoc.distanceSquaredTo(enemyArchon.location) > 45)
+						utils.tryMove(myLoc.directionTo(enemyArchon.location));
+					else
+						utils.tryMove(myLoc.directionTo(enemyArchon.location).opposite());
+				}
+				
+				else if(rc.isCoreReady()){
+					utils.tryMove(myLoc.directionTo(friends[rand.nextInt(friendCount)].location).opposite());
+				}
+				
+				
+				Clock.yield();
+			}
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 		
 	}
 
