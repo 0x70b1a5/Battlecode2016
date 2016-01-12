585da81b925871c947604a77247e5b7ef803739d
Brant <bpbiskup@hotmail.com>
2016-01-11 23:48:50 -0500

    Turrets pack up and move with group. Currently not repacking.

--- team042/Turret.java
+++ team042/Turret.java
@@ -35,33 +35,54 @@ public class Turret {
 	private void attack(boolean weakest) throws GameActionException {
 
 		MapLocation myLoc = rc.getLocation();
+		int myRange = rt.sensorRadiusSquared;
 		RobotInfo[] enemies = rc.senseHostileRobots(myLoc, rt.attackRadiusSquared);
+		RobotInfo[] friends= rc.senseNearbyRobots(myRange);
 		int enemyCount = enemies.length;
-
+		
 		// If this robot type can attack, check for enemies within range and attack one
-		if (rc.isCoreReady()&&enemyCount > 0&&rc.isWeaponReady()) {
-			// Find weakest enemy
-			RobotInfo target = enemies[0];
-
-
-			for (RobotInfo enemy : enemies) {
-				RobotInfo current = enemy;
-				if(weakest){
-					if (current.health < target.health) {
-						target = current;
-					}
-				}else{
-					if (current.health > target.health) {
-						target = current;
+		if (enemyCount > 0){
+			if (rt == RobotType.TTM) {
+				rc.unpack();
+				rt = RobotType.TURRET;
+			}
+			if (rc.isCoreReady() && rc.isWeaponReady()) {
+				// Find weakest enemy
+				RobotInfo target = enemies[0];
+	
+				for (RobotInfo enemy : enemies) {
+					RobotInfo current = enemy;
+					if(weakest){
+						if (current.health < target.health) {
+							target = current;
+						}
+					}else{
+						if (current.health > target.health) {
+							target = current;
+						}
 					}
 				}
-
-			}
-			if (rc.isWeaponReady()&&rc.canAttackLocation(target.location)) {
-				rc.attackLocation(target.location);
+				if (rc.isWeaponReady() && rc.canAttackLocation(target.location)) {
+					rc.attackLocation(target.location);
+				}
 			}
 		}
+		else if(enemyCount == 0){
+			if(rt == RobotType.TURRET){
+				rc.pack();
+				rt = RobotType.TTM;
+			}
+			for (RobotInfo bot : friends) {
+				// Group around archons first
+				if (bot.type == RobotType.ARCHON) {
+					utils.tryMove(myLoc.directionTo(bot.location).rotateLeft());
+				}
+				else{
+					utils.tryMove(myLoc.directionTo(bot.location).rotateRight());
+				}
+			}
+		}
+		
 		Clock.yield();
-
 	}
 }
